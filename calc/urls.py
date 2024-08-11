6fa14c0a97cbca5a21c1957944ba3e2346ded35c
EpicDeividProgram <mendozadavidprogramacion2.0@gmail.com>
2024-08-11 19:44:47 -0400

    actulizacion final de la calcuadora

--- calc/urls.py
+++ calc/urls.py
@@ -1,6 +1,10 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('save/', views.save_result, name='save_result'),
+    path('results/', views.get_results, name='get_results'),
+    path('most-used-operation/', views.most_used_operation, name='most_used_operation'),  # Nueva ruta para la vista de la operación más usada
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,17 +1,61 @@
+# calc/views.py
 
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CalculationResult
+from collections import Counter
 import numexpr as ne
 
+# Función principal de la calculadora
 def index(request):
     context = {}
     if request.method == 'POST':
         expression = request.POST.get('expression', '')
         try:
-            # Evaluar la expresión de manera segura
             result = ne.evaluate(expression).item()
             context['result'] = result
+            context['expression'] = expression
         except Exception as e:
             context['result'] = f"Error: {str(e)}"
     
     return render(request, 'calc/index.html', context)
+
+# Función para guardar los resultados en la base de datos
+def save_result(request):
+    if request.method == 'POST':
+        expression = request.POST.get('expression')
+        result = request.POST.get('result')
+        CalculationResult.objects.create(expression=expression, result=result)
+    return redirect('/')
+
+# Función para obtener los resultados almacenados en la base de datos
+def get_results(request):
+    results = CalculationResult.objects.all()
+    context = {'results': results}
+    return render(request, 'calc/results.html', context)
+
+def most_used_operation(request):
+    # Obtenemos todos los resultados almacenados en la base de datos
+    results = CalculationResult.objects.all()
+    operations = []
+
+    # Analizamos cada expresión para determinar qué operaciones se utilizaron
+    for result in results:
+        expression = result.expression
+        if '+' in expression:
+            operations.append('Suma (+)')
+        if '-' in expression:
+            operations.append('Resta (-)')
+        if '*' in expression:
+            operations.append('Multiplicación (*)')
+        if '/' in expression:
+            operations.append('División (/)')
+
+    # Contamos cuántas veces se ha utilizado cada operación
+    operation_counts = Counter(operations)
+
+    # Ordenamos por la operación más común
+    sorted_operations = operation_counts.most_common()
+
+    # Pasamos las operaciones ordenadas a la plantilla
+    context = {'sorted_operations': sorted_operations}
+    return render(request, 'calc/most_used_operation.html', context)
